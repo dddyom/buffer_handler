b0ac23188546693a196d8635ab464d9a6a11380a
dddyom <dddyom@gmail.com>
2021-07-28 13:24:33 +0600

    split matrix

--- first_split.py
+++ first_split.py
@@ -1,6 +1,30 @@
 import numpy as np
 
-matrix = np.load("/home/dddyom/temp/npy/SO_201127_151421.npy")
+from dev_config import PATH_TO_NPY_MATRIX as npy_path
 
-print(matrix.shape)
+def get_split_matrix(buffer_matrix, type_of_split='fst'):
+    width, length = buffer_matrix.shape
 
+    if type_of_split == 'fst':
+        width_result, length_result = 240, 256
+    elif type_of_split == 'snd':
+        width_result, length_result = 30, 128
+    else:
+        raise TypeError("Unexpected type of split")
+
+    result_split_matrix = buffer_matrix[0:width - width % width_result, 0:length - length % length_result].reshape(
+        width // width_result, width_result, -1, length_result).swapaxes(1, 2).reshape(-1, width_result, length_result)
+    return result_split_matrix
+
+
+def main():
+    test = np.load(npy_path)
+    print(test.shape, '\n')
+    split_matrix = get_split_matrix(test)
+    print(split_matrix.shape, '\n')
+    print(split_matrix[0])
+
+
+if __name__ == "__main__":
+    main()
+    pass
