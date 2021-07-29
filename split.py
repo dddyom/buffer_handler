6025868410f6f7ea947621629a0f97fa2398e04b
dddyom <dddyom@gmail.com>
2021-07-29 13:47:31 +0600

    coords to kilometers & grades

--- split.py
+++ split.py
@@ -3,28 +3,28 @@ import numpy as np
 from dev_config import PATH_TO_NPY_MATRIX as npy_path
 
 def get_split_matrix(buffer_matrix, type_of_split='fst'):
-    width, length = buffer_matrix.shape
+    distance, azimuth = buffer_matrix.shape
 
     if type_of_split == 'fst':
-        width_result, length_result = 240, 256
+        distance_result, azimuth_result = 240, 256
     elif type_of_split == 'snd':
-        width_result, length_result = 30, 128
+        distance_result, azimuth_result = 30, 128
     else:
         raise TypeError("Unexpected type of split")
 
-    result_split_matrix = buffer_matrix[0:width - width % width_result, 0:length - length % length_result].reshape(
-        width // width_result, width_result, -1, length_result).swapaxes(1, 2).reshape(-1, width_result, length_result)
+    result_split_matrix = buffer_matrix[0:distance - distance % distance_result, 0:azimuth - azimuth % azimuth_result].reshape(
+        distance // distance_result, distance_result, -1, azimuth_result).swapaxes(1, 2).reshape(-1, distance_result, azimuth_result)
     return result_split_matrix
 
 
 def get_null_coords_by_num(number, type_of_chunks='fst'):
     
     if type_of_chunks == 'fst':
-        width_indent, length_indent = 240, 256
+        distance_indent, azimuth_indent = 240, 256
         count_in_row = 5
         max_index = 39
     elif type_of_chunks == 'snd':
-        width_indent, length_indent = 30, 128
+        distance_indent, azimuth_indent = 30, 128
         count_in_row = 8
         max_index = 15
     else:
@@ -33,28 +33,41 @@ def get_null_coords_by_num(number, type_of_chunks='fst'):
     if number >= (max_index + 1) or number <= -1:
         raise IndexError("Unexpected index of chunk")
     
-    width_coordinate = width_indent * (number % count_in_row)
+    distance_coordinate = distance_indent * (number % count_in_row)
     
-    length_coordinate = length_indent *(number // count_in_row)    
+    azimuth_coordinate = azimuth_indent *(number // count_in_row)    
     
-    #length_coordinate = 0    
+    #azimuth_coordinate = 0    
     #for i in range(-1, number, count_in_row):
         #if i == -1:
             #continue
-        #length_coordinate += length_indent
+        #azimuth_coordinate += azimuth_indent
         #print(i)
         
-    return (width_coordinate, length_coordinate)
+    return (distance_coordinate, azimuth_coordinate)
 
-def get_center_by_null_coords(width_null_coord, length_null_coord):
-    width_indent = 30
-    length_indent = 128
+def get_center_by_null_coords(distance_null_coord, azimuth_null_coord):
+    distance_indent = 30
+    azimuth_indent = 128
     
-    width_center_coord = width_null_coord + width_indent // 2
-    length_center_coord = length_null_coord + length_indent // 2
+    distance_center_coord = distance_null_coord + distance_indent // 2
+    azimuth_center_coord = azimuth_null_coord + azimuth_indent // 2
     
-    return (width_center_coord, length_center_coord)
+    return (distance_center_coord, azimuth_center_coord)
     
+def get_grades_and_kilometers_by_coords(distance_coord, azimuth_coord):
+    
+    kilometers = int((distance_coord / 2048) * 360)
+    grades = int((azimuth_coord / 1200) * 360)
+    
+    return kilometers, grades
+
+#def kilometers_and_grades_to_coordinates(grades, kilometers):
+    #result_of_grades = int(grades * 2048 / 360)
+    #result_of_kilometers = int(kilometers * 1200 / 360)
+    #return result_of_grades, result_of_kilometers
+
+
 
 def main():
     test = np.load(npy_path)
@@ -62,10 +75,14 @@ def main():
     split_matrix = get_split_matrix(test)
     #print(split_matrix.shape, '\n')
     #print(split_matrix[0])
-    width_null, length_null = get_null_coords_by_num(number=39)
-    #print(get_center_by_null_coords(width_null, length_null))
+    distance_null, azimuth_null = get_null_coords_by_num(number=39)
+    print(get_grades_and_kilometers_by_coords(distance_null, azimuth_null))
+    print(get_center_by_null_coords(distance_null, azimuth_null))
 
 
 if __name__ == "__main__":
     main()
     pass
+
+
+
