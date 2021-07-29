2d81be3cabe73ed1d6b958652706851e478da26f
dddyom <dddyom@gmail.com>
2021-07-29 11:39:26 +0600

    mv fist_split split

--- split.py
+++ split.py
@@ -17,12 +17,29 @@ def get_split_matrix(buffer_matrix, type_of_split='fst'):
     return result_split_matrix
 
 
+def get_null_coords_by_num(number=3, type_of_chunks='fst'):
+    
+    if type_of_chunks == 'fst':
+        width_indent, length_indent = 240, 256
+        count_in_row = 5
+    elif type_of_chunks == 'snd':
+        width_indent, length_indent = 30, 128
+        count_in_row = 8
+    
+    width_axis = 0    
+    for i in range(0, number, count_in_row):
+        width_axis += width_indent
+    
+    length_axis = length_indent * (number % count_in_row)
+    return (width_axis, length_axis)
+
 def main():
     test = np.load(npy_path)
     print(test.shape, '\n')
     split_matrix = get_split_matrix(test)
     print(split_matrix.shape, '\n')
     print(split_matrix[0])
+    print(get_null_coords_by_num(number=39))
 
 
 if __name__ == "__main__":
