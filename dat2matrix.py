0244cfd6c0db334dc083ec806ac56f5ffddf816d
dddyom <dddyom@gmail.com>
2021-08-18 13:32:15 +0600

    dat2np

--- dat2matrix.py
+++ dat2matrix.py
@@ -4,16 +4,11 @@ import numpy as np
 f = open("sources/SO_201207_153155.dat", "rb")
 buffer = f.read()
 
-temp = []
-
-for i in range(len(buffer)):
-    temp.append(buffer[i])
-
 matrix = []
 
 for i in range(0, len(buffer)-1, 2):
     matrix.append(int(str(buffer[i + 1]) + str(buffer[i])))
-    #matrix.append(int(str(buffer[i]) + str(buffer[i + 1])))
+
 
 matrix = np.array(matrix)
 print(matrix.shape)
