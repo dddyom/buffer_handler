bde38ca702b6be2a82d25f6c86041e2a5000c9d4
dddyom <dddyom@gmail.com>
2021-08-25 14:46:42 +0600

    set stopwatch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 import matrix_tools
 from buffer_handler_init import DAT_PATH, CHECKPOINT_PATH
@@ -31,31 +32,53 @@ except Exception:
 
 
 def main():
+    
+    load_fst_neural_start = time.perf_counter()
     first_trained_neural = Neural(
         240, 256, checkpoint_path + "/first_trained.ckpt")
+    print("First neural boot time: %.2f" % (time.perf_counter() - load_fst_neural_start))
+
+    load_snd_neural_start = time.perf_counter()
     second_trained_neural = Neural(
         30, 128, checkpoint_path + "/second_trained.ckpt")
+    print("Second neural boot time: %.2f" % (time.perf_counter() - load_snd_neural_start))
 
+
+    loading_dat_file_list_start = time.perf_counter()
     all_files = os.listdir(dat_path)
     all_dat = filter(lambda x: x.endswith('.dat'), all_files)
+    print("Loading dat file list time: %.2f" % (time.perf_counter() - loading_dat_file_list_start))
     for i in all_dat:
+        convert_dat_to_numpy_start = time.perf_counter()
         np_matrix = dat2nparr(dat_path + "/" + i)
+        print("Convert dat file to numpy for %(1)s time: %(2).2f" % {"1": i, "2": (time.perf_counter() - convert_dat_to_numpy_start)})
+
+        split_fst_start = time.perf_counter()
         first_split_matrix = matrix_tools.get_split_matrix(np_matrix)
+        print("First split time for %(1)s is: %(2).2f" % {"1": i, "2": (time.perf_counter() - split_fst_start)})
+        predict_fst_start = time.perf_counter()
         first_trained_neural.predict(first_split_matrix)
+        print("First predict time for %(1)s is: %(2).2f" % {"1": i, "2": (time.perf_counter() - predict_fst_start)})
         # print(first_trained_neural.sug_targets_ind)
-        for i in range(len(first_split_matrix)):
-            second_split_matrix = matrix_tools.get_split_matrix(first_split_matrix[i], type_of_split="snd")
+        second_split_and_predict_start = time.perf_counter()
+        sug_targets_before_first = first_trained_neural.sug_targets
+        for j in range(len(sug_targets_before_first)):
+            second_split_matrix = matrix_tools.get_split_matrix(sug_targets_before_first[j], type_of_split="snd")
             second_trained_neural.predict(second_split_matrix)
+
+        print("Second split and predict time for %(1)s is : %(2).2f" % {"1": i, "2": (time.perf_counter() - second_split_and_predict_start)})
         # print(second_trained_neural.sug_targets_ind)
-        for i in second_trained_neural.sug_targets_ind:
+        convert_coordinates_start = time.perf_counter()
+        for k in second_trained_neural.sug_targets_ind:
             # print(first_trained_neural.sug_targets_ind[i], i)
             distance_null_coord, azimuth_null_coord = matrix_tools.get_null_coords_by_num(
-                first_trained_neural.sug_targets_ind[i], i)
+                first_trained_neural.sug_targets_ind[k], k)
             distance_center_coord, azimuth_center_coord = matrix_tools.get_center_by_null_coords(distance_null_coord,
                                                                                                  azimuth_null_coord)
             kilometers, grades = matrix_tools.get_kilometers_and_grades_by_coords(distance_center_coord,
                                                                                   azimuth_center_coord)
             print(kilometers, grades)
+        print("Convert coordinates time for %(1)s is %(2).2f" % {"1": i, "2": (time.perf_counter() - convert_coordinates_start)})
 
     #     first_np_targets, first_targets_i = first_handle(matrix)
     #     second_targets_i = second_handle(first_np_targets, first_targets_i)
